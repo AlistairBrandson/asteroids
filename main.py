ce996c4d2e6c997e1e40fb3517fc3c5be10a3898
AlistairBrandson <alistair.brandson@gmail.com>
2024-12-31 07:43:31 +0000

    Re-organised and commented main.py. Fixed speed issue, dt variable in main loop was adding rather than just updating

--- main.py
+++ main.py
@@ -9,22 +9,27 @@ from asteroid import *
 from asteroidfield import *
 
 def main():
+    # Welcome banner in terminal when running main.py
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Define and initialize variables
+    screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
+    # Add all class containers to defined PyGame groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
 
-    screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    # Create asteroid field and player objects
     asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -32,7 +37,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+            
+        screen.fill("black")
 
         for item in updatable:
             item.update(dt)
@@ -42,7 +48,7 @@ def main():
 
 
         pygame.display.flip()
-        dt += clock.tick(60)/1000
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
