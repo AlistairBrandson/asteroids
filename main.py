23a903e1288cb1536ca854360a908b4009238b86
AlistairBrandson <alistair.brandson@gmail.com>
2024-12-31 07:18:02 +0000

    Containerised Player and added Groups

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     clock = pygame.time.Clock()
@@ -23,9 +28,12 @@ def main():
                 return
         screen.fill(000000)
 
-        
-        player.draw(screen)
-        player.update(dt)
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
 
         pygame.display.flip()
         dt += clock.tick(60)/1000
